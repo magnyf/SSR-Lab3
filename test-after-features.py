b3e89adc397b5f57e56251b36ccd9749f6e055cb
Unknown <laabyounes@hotmail.com>
2018-05-11 15:40:40 +0200

    Premiers tests de deeplearning avec dataset simplifié
    pour l'instant la plupart des paramètres sont mis au pif mais ca fonctionne, reste plus qu'à mieux comprendre les paramètres et lancer ca sur le PDC avec la database complete

--- test-after-features.py
+++ test-after-features.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as pl
 from sklearn.mixture import *
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
 
 def path2info(path):
     """
@@ -26,22 +29,23 @@ nstates = {phone: phoneHMMs[phone]['means'].shape[0] for phone in phones}
 stateList = [ph + '_' + str(id) for ph in phones for id in range(nstates[ph])]
 
 ## Whole datasets
-traindata = np.load('traindata.npz')['traindata']
-testdata = np.load('testdata.npz')['testdata']
+# traindata = np.load('traindata.npz')['traindata']
+# testdata = np.load('testdata.npz')['testdata']
+# ### To save memory it was better to save the index rather than the name of the state,
+# ## I didn't do it when generating the file so I fix this here
+# ## TODO save the file again after the changes to save time afterwards
 
-### Smaller sets to improve speed
-# traindata = np.load('traindata.npz')['traindata'][:1000]
-# testdata = np.load('testdata.npz')['testdata'][:1000]
+# for x in traindata:
+#     x['targets'] = [stateList.index(y) for y in x['targets']]
+# for x in testdata:
+#     x['targets'] = [stateList.index(y) for y in x['targets']]
 
 
-### To save memory it was better to save the index rather than the name of the state,
-## I didn't do it when generating the file so I fix this here
-## TODO save the file again after the changes to save time afterwards
+### Smaller sets to improve speed (the dataset is lame after this, no diversity)
+traindata = np.load('traindataSimple.npz')['traindata']
+testdata = np.load('testdataSimple.npz')['testdata']
 
-for x in traindata:
-    x['targets'] = [stateList.index(y) for y in x['targets']]
-for x in testdata:
-    x['targets'] = [stateList.index(y) for y in x['targets']]
+
 
 ################
 ### TRAINING SET
@@ -201,6 +205,22 @@ print(len(mspec_test_x))
 print(len(lmfcc_test_x))
 print(len(test_y))
 
+### Smaller sets to improve speed: idea divide by 100 the size
+arrays = [mspec_train_x, lmfcc_train_x, 
+mspec_val_x, lmfcc_val_x, 
+mspec_test_x, lmfcc_test_x,
+train_y, 
+val_y,
+test_y]
+
+for array in arrays:
+    array = array[::100]
+print('test set size')
+print(len(mspec_test_x))
+
+
+print('Feature extraction DONE')
+
 
 
 ############
@@ -209,6 +229,8 @@ print(len(test_y))
 
 dmspec_train_x = []
 dlmfcc_train_x = []
+dmspec_val_x = []
+dlmfcc_val_x = []
 dmspec_test_x = []
 dlmfcc_test_x = []
 
@@ -225,6 +247,20 @@ for i in range(len(mspec_train_x)):
     dmspec_train_x += [tempMspec]
     dlmfcc_train_x += [tempLmfcc]
 
+for i in range(len(mspec_val_x)):
+    tempMspec = []
+    tempLmfcc = []
+    n = len(mspec_val_x)
+    for j in range(-3, 4):
+        tempMspec += list(mspec_val_x[(i+j)%n])
+        tempLmfcc += list(lmfcc_val_x[(i+j)%n])
+    assert(len(tempMspec) == len(mspec_val_x[0])*7)
+    assert(len(tempLmfcc) == len(lmfcc_val_x[0])*7)
+
+    dmspec_val_x += [tempMspec]
+    dlmfcc_val_x += [tempLmfcc]
+
+
 for i in range(len(mspec_test_x)):
     tempMspec = []
     tempLmfcc = []
@@ -238,6 +274,7 @@ for i in range(len(mspec_test_x)):
     dmspec_test_x += [tempMspec]
     dlmfcc_test_x += [tempLmfcc]
 
+print('Dynamic features DONE')
 
 #################
 # Standardisation
@@ -259,6 +296,16 @@ dmspec_train_x = scaler_dmspec.fit_transform(dmspec_train_x)
 # print(scalerTest_lmfcc.mean_)
 # print(scalerTest_lmfcc.var_)
 
+# transform for the other sets
+lmfcc_val_x = scaler_lmfcc.transform(lmfcc_val_x)
+dlmfcc_val_x = scaler_dlmfcc.transform(dlmfcc_val_x)
+mspec_val_x = scaler_mspec.transform(mspec_val_x)
+dmspec_val_x = scaler_dmspec.transform(dmspec_val_x)
+
+lmfcc_test_x = scaler_lmfcc.transform(lmfcc_test_x)
+dlmfcc_test_x = scaler_dlmfcc.transform(dlmfcc_test_x)
+mspec_test_x = scaler_mspec.transform(mspec_test_x)
+dmspec_test_x = scaler_dmspec.transform(dmspec_test_x)
 
 #### Format change to use Keras and a GPU
 
@@ -267,8 +314,90 @@ dlmfcc_train_x = dlmfcc_train_x.astype('float32')
 mspec_train_x = mspec_train_x.astype('float32')
 dmspec_train_x = dmspec_train_x.astype('float32')
 
+lmfcc_val_x = lmfcc_val_x.astype('float32')
+dlmfcc_val_x = dlmfcc_val_x.astype('float32')
+mspec_val_x = mspec_val_x.astype('float32')
+dmspec_val_x = dmspec_val_x.astype('float32')
+
+lmfcc_test_x = lmfcc_test_x.astype('float32')
+dlmfcc_test_x = dlmfcc_test_x.astype('float32')
+mspec_test_x = mspec_test_x.astype('float32')
+dmspec_test_x = dmspec_test_x.astype('float32')
+
 output_dim = len(stateList)
+
+print(output_dim)
 
 train_y = np_utils.to_categorical(train_y, output_dim)
 val_y = np_utils.to_categorical(val_y, output_dim)
 test_y = np_utils.to_categorical(test_y, output_dim)
+
+print('Standardisation DONE')
+
+
+##### PART 5
+## TODO
+# Define the proper size for the input and output layers depending on your feature vectors 
+# and number of states. Choose the appropriate activation function for the output layer, 
+# given that you want to perform classification. Be prepared to explain why you chose the 
+# specific activation and what alternatives there are. For the intermediate layers you can
+# choose, for example, between relu and sigmoid activation functions.
+
+#### toutes les options sont choisies au pif pour l'instant
+def trainAndEvaluate(typeFeature, numberOfLayers=1, epoch=10):
+    print()
+    print()
+    print('----------'+typeFeature+' with '+str(numberOfLayers)+' layer-----------')
+
+    if (typeFeature == "lmfcc"):
+        input_dim = 13
+        train_x = lmfcc_train_x
+        test_x = lmfcc_test_x
+        val_x = lmfcc_val_x
+    elif (typeFeature == "dlmfcc"):
+        input_dim = 91
+        train_x = dlmfcc_train_x
+        test_x = dlmfcc_test_x
+        val_x = dlmfcc_val_x
+    elif (typeFeature == "mspec"):
+        input_dim = 40
+        train_x = mspec_train_x
+        test_x = mspec_test_x
+        val_x = mspec_val_x
+    elif (typeFeature == "dmspec"):
+        input_dim = 280
+        train_x = dmspec_train_x
+        test_x = dmspec_test_x
+        val_x = dmspec_val_x
+
+    model = Sequential()
+
+    model.add(Dense(256, input_dim=input_dim))
+    model.add(Activation('relu'))
+
+    for i in range(numberOfLayers-1):
+        model.add(Dense(256))
+        model.add(Activation('relu'))
+
+    model.add(Dense(output_dim))
+    model.add(Activation('relu'))
+
+
+    ## in the documentation these are the ones adapted to a multi-class classification problem
+    model.compile(optimizer='sgd',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+
+    history = model.fit(train_x, train_y, epochs=10, batch_size=256, validation_data=(val_x, val_y), verbose=2)
+    # validation_data: tuple (val_x, y_val) or tuple (val_x, y_val, val_sample_weights) on which to evaluate the loss and any model metrics at the end of each epoch. The model will not be trained on this data. 
+
+    print(history.history)
+
+    evaluation = model.evaluate(x=test_x, y=test_y, batch_size=256, steps=None, verbose=1)
+
+    print(evaluation)
+
+for typeFeature in ["lmfcc","dlmfcc","mspec","dmspec"]:
+    for numberOfLayers in range(1,5):
+        trainAndEvaluate(typeFeature, numberOfLayers)
